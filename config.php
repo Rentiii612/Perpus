fe698d2f53703273f21fdbc56a462dfe61f1268f
Rentiii612 <yorentianarafu@gmail.com>
2025-08-03 01:29:51 +0700

    Yorentiana Rafu

--- config.php
+++ config.php
@@ -1,13 +1,6 @@
 <?php
-$host = 'localhost';     // ganti jika berbeda
-$dbname = 'Perpus'; // sesuaikan dengan nama database MySQL kamu
-$user = 'root';          // default user XAMPP
-$pass = '';              // default kosong (jika pakai password, isikan di sini)
-
-try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Koneksi database gagal: " . $e->getMessage());
+$koneksi = new mysqli("localhost", "root", "", "Perpus");
+if ($koneksi->connect_error) {
+    die("Koneksi gagal: " . $koneksi->connect_error);
 }
 ?>
